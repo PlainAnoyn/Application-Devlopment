8ca4444c9a431089b7971375569d865443a7fed5
Xebec <Xebec@NITRO>
2024-12-16 11:38:23 +0545

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Application_Devlopment
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Application_Devlopment
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
